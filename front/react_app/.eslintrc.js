0bc4680d08320e4df81bb5affb45108098bff837
katkatprog <katkatprog@gmail.com>
2022-10-30 15:31:40 +0900

    ESLintの導入

---
+++ front/react_app/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-uses-react": "off", //「React' must be in scope when using JSX」を防止
+    "react/react-in-jsx-scope": "off", //「React' must be in scope when using JSX」を防止
+  },
+  // React version not specified in eslint-plugin-react settings...を防止
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
