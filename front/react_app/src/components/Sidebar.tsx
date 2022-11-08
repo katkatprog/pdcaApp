1eae648f94b50c114f1dbb5a00568b54087c22dc
katkatprog <katkatprog@gmail.com>
2022-11-09 01:12:59 +0900

    サイドバー 内容記載

--- front/react_app/src/components/Sidebar.tsx
+++ front/react_app/src/components/Sidebar.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const StyledSidebar = styled.aside`
   background-color: #eff3f4;
   flex: 1;
   min-height: 100vh;
+  div {
+    margin: 25px;
+  }
+  p {
+    margin: 10px auto;
+    padding: 8px;
+    border-radius: 5px;
+    &:hover {
+      background-color: #cacaca;
+      transition: all 0.4s;
+    }
+  }
 `;
 
 const Sidebar = () => {
-  return <StyledSidebar>sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <div>
+        <p>kat</p>
+        <Link to="/">
+          <p>作成したサイクル</p>
+        </Link>
+        <p>お気に入り</p>
+        <Link to="/trash">
+          <p>消去したサイクル</p>
+        </Link>
+        <p>サインアウト</p>
+      </div>
+    </StyledSidebar>
+  );
 };
 
 export default Sidebar;
