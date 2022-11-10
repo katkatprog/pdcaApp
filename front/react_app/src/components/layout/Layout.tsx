e7017418427016ca611479c9a2643827d63cd5d1
katkatprog <katkatprog@gmail.com>
2022-11-11 00:44:49 +0900

    Layoutをtailwind化

--- front/react_app/src/components/layout/Layout.tsx
+++ front/react_app/src/components/layout/Layout.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import Sidebar from "../Sidebar";
 
 interface MainProps {
   children: React.ReactNode;
 }
 
-const StyledLayout = styled.div`
-  display: flex;
-`;
-
 const Layout = (props: MainProps) => {
   return (
     <>
-      <StyledLayout>
+      <div className="flex">
         <Sidebar></Sidebar>
         <main className="w-4/5">{props.children}</main>
-      </StyledLayout>
+      </div>
     </>
   );
 };
