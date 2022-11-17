e78276771f581a34cd992568d52491e65187f064
katkatprog <katkatprog@gmail.com>
2022-11-17 15:19:01 +0000

    サイクルのupdateメソッドのdto変更

--- api/nest_app/src/cycles/cycle.dto.ts
+++ api/nest_app/src/cycles/cycle.dto.ts
@@ -22,16 +22,17 @@ export class CycleDto {
 }
 
 // 編集時のDTO
-export class CycleDtoEdit extends CycleDto {
-  @Type(() => Boolean)
-  @IsNotEmpty()
-  favorite: boolean;
+export class CycleDtoEdit {
+  @IsString({ message: 'サイクル名は文字列で入力してください。' })
+  @IsNotEmpty({ message: 'サイクル名は必須項目です。' })
+  @MaxLength(20, { message: 'サイクル名は20文字以内で入力してください。' })
+  name: string;
 
-  @Type(() => Boolean)
-  @IsNotEmpty()
-  watchFromAnyone: boolean;
+  @IsString({ message: 'サイクルの概要は文字列で入力してください。' })
+  about: string;
 
-  @Type(() => Boolean)
-  @IsNotEmpty()
-  suspend: boolean;
+  @IsString({ message: 'サイクルの目標は文字列で入力してください。' })
+  @IsNotEmpty({ message: 'サイクルの目標は必須項目です。' })
+  @MaxLength(40, { message: 'サイクルの目標は40文字以内で入力してください。' })
+  goal: string;
 }
