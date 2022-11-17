87919e3347f24273f7b7fa98c1f6592ab6d102ce
katkatprog <katkatprog@gmail.com>
2022-11-17 14:35:10 +0000

    cyclesのリクエストの引数に、userIdを指定。また、URIをParamとBodyだけで表現

--- api/nest_app/src/cycles/cycles.controller.ts
+++ api/nest_app/src/cycles/cycles.controller.ts
@@ -7,7 +7,6 @@ import {
   ParseIntPipe,
   Post,
   Put,
-  Query,
 } from '@nestjs/common';
 import { CycleDto, CycleDtoEdit } from './cycle.dto';
 import { CycleIfc } from './cycle.interface';
@@ -16,26 +15,29 @@ import { CyclesService } from './cycles.service';
 @Controller('cycles')
 export class CyclesController {
   constructor(private readonly cyclesService: CyclesService) {}
-  @Get('')
+  @Get(':userId')
   async findAll(
-    @Query('userId', ParseIntPipe) userId: number,
+    @Param('userId', ParseIntPipe) userId: number,
   ): Promise<CycleIfc[]> {
     return await this.cyclesService.findAll(userId);
   }
 
   // 消去されたサイクル一覧
-  @Get('trashed')
+  @Get('trashed/:userId')
   async findTrashedCycles(
-    @Query('userId', ParseIntPipe) userId: number,
+    @Param('userId', ParseIntPipe) userId: number,
   ): Promise<CycleIfc[]> {
     return await this.cyclesService.findTrashedCycles(userId);
   }
 
   // サイクルの詳細を取得 サイクルのIDをURLパラメータに表示させるため、
   // 上記のメソッドとかぶらないよう、Getの一番下に記載している。
-  @Get(':id')
-  async findById(@Param('id', ParseIntPipe) id: number): Promise<CycleIfc> {
-    return await this.cyclesService.findById(id);
+  @Get(':id/:userId')
+  async findById(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
+  ): Promise<CycleIfc> {
+    return await this.cyclesService.findById(id, userId);
   }
 
   @Post('create')
@@ -43,29 +45,39 @@ export class CyclesController {
     return await this.cyclesService.create(cycleDto);
   }
 
-  @Put('update/:id')
+  @Put('update/:id/:userId')
   async update(
     @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
     @Body() cycleDtoEdit: CycleDtoEdit,
   ): Promise<void> {
-    return await this.cyclesService.update(id, cycleDtoEdit);
+    return await this.cyclesService.update(id, userId, cycleDtoEdit);
   }
 
   // サイクルを消去する(erasedをtrueに変更する)処理
-  @Put('erase/:id')
-  async erase(@Param('id', ParseIntPipe) id: number) {
-    return await this.cyclesService.erase(id);
+  @Put('erase/:id/:userId')
+  async erase(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
+  ) {
+    return await this.cyclesService.erase(id, userId);
   }
 
   // 消去されたサイクルを復元する(erasedをfalseに変更する)処理
-  @Put('restore/:id')
-  async restore(@Param('id', ParseIntPipe) id: number) {
-    return await this.cyclesService.restore(id);
+  @Put('restore/:id/:userId')
+  async restore(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
+  ) {
+    return await this.cyclesService.restore(id, userId);
   }
 
   // サイクルの完全な削除
-  @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number) {
-    return await this.cyclesService.delete(id);
+  @Delete(':id/:userId')
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
+  ) {
+    return await this.cyclesService.delete(id, userId);
   }
 }
