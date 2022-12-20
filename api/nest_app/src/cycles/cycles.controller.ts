bce5b4198e29db979609570ff74182edf095296b
katkatprog <katkatprog@gmail.com>
2022-12-20 14:08:50 +0000

    p,d,c,aの各データの作成処理を実装

--- api/nest_app/src/cycles/cycles.controller.ts
+++ api/nest_app/src/cycles/cycles.controller.ts
@@ -9,12 +9,23 @@ import {
   Put,
 } from '@nestjs/common';
 import { Cycle } from '@prisma/client';
+import { ActionsService } from 'src/actions/actions.service';
+import { ChecksService } from 'src/checks/checks.service';
+import { DosService } from 'src/dos/dos.service';
+import { PlansService } from 'src/plans/plans.service';
 import { CycleDto, CycleDtoEdit } from './cycle.dto';
 import { CyclesService } from './cycles.service';
 
 @Controller('cycles')
 export class CyclesController {
-  constructor(private readonly cyclesService: CyclesService) {}
+  constructor(
+    private readonly cyclesService: CyclesService,
+    private readonly plansService: PlansService,
+    private readonly dosService: DosService,
+    private readonly checksService: ChecksService,
+    private readonly actionsService: ActionsService,
+  ) {}
+
   @Get(':userId')
   async findAll(
     @Param('userId', ParseIntPipe) userId: number,
@@ -43,6 +54,19 @@ export class CyclesController {
   @Post('create')
   async create(@Body() cycleDto: CycleDto): Promise<void> {
     return await this.cyclesService.create(cycleDto);
+  }
+
+  @Post('create-pdca/:cycleId/:round')
+  async createPDCA(
+    @Param('cycleId', ParseIntPipe) cycleId: number,
+    @Param('round', ParseIntPipe) round: number,
+  ): Promise<void> {
+    Promise.all([
+      this.plansService.createPlan(cycleId, round),
+      this.dosService.createDo(cycleId, round),
+      this.checksService.createCheck(cycleId, round),
+      this.actionsService.createAction(cycleId, round),
+    ]);
   }
 
   @Put('update/:id/:userId')
