96c446a1dc50c8906f3b350a3f9287c2d6d60bbf
katkatprog <katkatprog@gmail.com>
2023-01-03 07:27:33 +0000

    ページネーションを実装

--- front/react_app/src/pages/cycle/Cycle.tsx
+++ front/react_app/src/pages/cycle/Cycle.tsx
@@ -1,3 +1,5 @@
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,7 +15,9 @@ const Cycle = () => {
   const cycle = useSelector((state: RootState) => state.cycle.value);
   const dispatch = useDispatch();
   const [mode, setMode] = useState<string>("about");
-  const [round, setRound] = useState<number>(0);
+  const [latestRound, setLatestRound] = useState<number>(0);
+  const [selectedRound, setSelectedRound] = useState<number>(0);
+  const [roundArray, setRoundArray] = useState<number[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -25,13 +29,51 @@ const Cycle = () => {
       const fetchedRound: number = await (
         await axios.get(`/api/plans/latest-round/${params.cycleId}`)
       ).data;
-      setRound(fetchedRound);
+      setLatestRound(fetchedRound);
+      setSelectedRound(fetchedRound);
+
+      // 最新のroundまでが入った整数の配列(ページネーションに使用)
+      updateRoundArray(fetchedRound, fetchedRound);
     })();
 
     return () => {
       dispatch(refreshCycle());
     };
   }, []);
+
+  // ページ番号が格納された配列を作る関数(ページネーションに使用)
+  const updateRoundArray = (selectedRound: number, latestRound: number) => {
+    const numberOfShowRounds = 5; //ページネーションで画面表示させるラウンド数
+    const tmpRoundArray: number[] = []; //表示させるラウンドを格納する配列
+    if (latestRound < numberOfShowRounds) {
+      // ページ数が少ない場合
+      for (let i = 1; i <= latestRound; i++) {
+        tmpRoundArray.push(i);
+      }
+    } else {
+      // ページ数が多い場合 → numOfShowRoundsのページ数に留める
+      const halfOfShowRounds = Math.floor(numberOfShowRounds / 2); //ページネーションで表示させるラウンド数の半分
+
+      if (selectedRound <= halfOfShowRounds) {
+        for (let i = 1; i <= numberOfShowRounds; i++) {
+          tmpRoundArray.push(i);
+        }
+      } else if (selectedRound > latestRound - halfOfShowRounds) {
+        for (let i = latestRound; i > latestRound - numberOfShowRounds; i--) {
+          tmpRoundArray.push(i);
+        }
+      } else {
+        tmpRoundArray.push(selectedRound);
+        for (let i = 1; i <= halfOfShowRounds; i++) {
+          tmpRoundArray.push(selectedRound + i);
+          tmpRoundArray.push(selectedRound - i);
+        }
+      }
+    }
+
+    tmpRoundArray.sort((a, b) => a - b);
+    setRoundArray(tmpRoundArray);
+  };
 
   return (
     <>
@@ -57,6 +99,32 @@ const Cycle = () => {
             </div>
             <div>
               <h1>Action</h1>
+            </div>
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faAngleLeft}
+                className="text-3xl"
+              ></FontAwesomeIcon>
+              {roundArray.map((round) => (
+                <span
+                  className={`p-2 rounded-md text-slate-50 mx-1 select-none ${
+                    round === selectedRound
+                      ? "bg-gray-600"
+                      : "bg-gray-400 cursor-pointer"
+                  }`}
+                  key={round}
+                  onClick={() => {
+                    setSelectedRound(round);
+                    updateRoundArray(round, latestRound);
+                  }}
+                >
+                  {round}
+                </span>
+              ))}
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                className="text-3xl"
+              ></FontAwesomeIcon>
             </div>
           </article>
           {mode === "about" && <AboutCycle></AboutCycle>}
