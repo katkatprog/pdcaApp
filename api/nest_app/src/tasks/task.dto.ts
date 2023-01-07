6ffb38bbe35f469d537228254b6991dd04c80020
katkatprog <katkatprog@gmail.com>
2023-01-07 14:39:52 +0000

    タスク編集モーダルから、日付を選択できるよう改修

--- api/nest_app/src/tasks/task.dto.ts
+++ api/nest_app/src/tasks/task.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class TaskDto {
   @IsNotEmpty({ message: 'タスク名を入力してください。' })
@@ -8,13 +9,11 @@ export class TaskDto {
   @IsString({ message: '概要は文字列である必要があります。' })
   about: string;
 
+  @Type(() => Date)
+  @IsDate({ message: '開始日は日付型である必要があります。' })
   startDate: Date;
-  endDate: Date;
-  // @Type(() => Date)
-  // @IsDate({ message: '開始日は日付型である必要があります。' })
-  // startDate: Date;
 
-  // @Type(() => Date)
-  // @IsDate({ message: '終了日は日付型である必要があります。' })
-  // endDate: Date;
+  @Type(() => Date)
+  @IsDate({ message: '終了日は日付型である必要があります。' })
+  endDate: Date;
 }
