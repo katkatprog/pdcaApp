6ffdb0bb84ee89381db416bda86861f9888ea12e
katkatprog <katkatprog@gmail.com>
2023-01-08 14:31:58 +0000

    modalのsliceを削除

--- front/react_app/src/redux/store.ts
+++ front/react_app/src/redux/store.ts
@@ -3,12 +3,10 @@ import { cycleSlice } from "./cycleSlice";
 import { cyclesSlice } from "./cyclesSlice";
 import { erasedCyclesSlice } from "./erasedCyclesSlice";
 import { menuSlice } from "./menuSlice";
-import { modalSlice } from "./modalSlice";
 
 export const store = configureStore({
   reducer: {
     cycle: cycleSlice.reducer,
-    modal: modalSlice.reducer,
     cycles: cyclesSlice.reducer,
     erasedCycles: erasedCyclesSlice.reducer,
     menu: menuSlice.reducer,
