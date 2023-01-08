9b5e1bc689daa7be45dd1bb7e73e1ec09bb23c31
katkatprog <katkatprog@gmail.com>
2023-01-08 11:39:15 +0000

    taskの編集モーダルのオーバーレイに、切り出したModalOverlayを適用

--- front/react_app/src/pages/cycle/task/EditTaskModal.tsx
+++ front/react_app/src/pages/cycle/task/EditTaskModal.tsx
@@ -1,26 +1,101 @@
-import React, { ReactNode } from "react";
+import { Task } from "@prisma/client";
+import React, { useState } from "react";
+import ModalOverlay from "../../../components/modal/ModalOverlay";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 interface Props {
   closeModalAction: () => void;
-  children: ReactNode;
+  task: Task;
+  setTask: React.Dispatch<React.SetStateAction<Task>>;
 }
 
 const EditTaskModal = (props: Props) => {
+  const [nameOnModal, setNameOnModal] = useState(props.task.name);
+  const [aboutOnModal, setAboutOnModal] = useState(props.task.about);
+  const [startDateOnModal, setStartDateOnModal] = useState(
+    props.task.startDate ? new Date(props.task.startDate) : null,
+  );
+  const [endDateOnModal, setEndDateOnModal] = useState(
+    props.task.endDate ? new Date(props.task.endDate) : null,
+  );
+
   return (
     <>
-      <div
-        onClick={() => props.closeModalAction()}
-        className="bg-black/[.3] h-full w-full fixed inset-0 flex items-center justify-center z-10"
+      <ModalOverlay
+        modalWidth="w-1/2"
+        closeModalAction={props.closeModalAction}
       >
-        <div
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-          className="bg-slate-50 px-8 py-8 flex flex-col justify-center items-center rounded-lg w-3/6"
-        >
-          <div className="w-full">{props.children}</div>
+        <div className="p-12">
+          <h1 className="text-xl">タスクの編集</h1>
+          <p className="my-2">タスク名</p>
+          <input
+            type="text"
+            className="w-full p-2 rounded-md bg-slate-200 border-none outline-none"
+            value={nameOnModal}
+            onChange={(e) => setNameOnModal(e.target.value)}
+            required
+          />
+          <p className="my-2">概要</p>
+          <input
+            type="text"
+            className="w-full p-2 rounded-md bg-slate-200 border-none outline-none"
+            value={aboutOnModal || ""}
+            onChange={(e) => setAboutOnModal(e.target.value)}
+            required
+          />
+          <p className="my-2">期間</p>
+          <div>
+            <DatePicker
+              className="w-1/3 p-2 rounded-md bg-slate-200 border-none outline-none"
+              dateFormat="yyyy/MM/dd"
+              selected={startDateOnModal}
+              onChange={(date) => {
+                setStartDateOnModal(date);
+              }}
+            ></DatePicker>
+            <span className="mx-2 text-xl">~</span>
+            <DatePicker
+              className="w-1/3 p-2 rounded-md bg-slate-200 border-none outline-none"
+              dateFormat="yyyy/MM/dd"
+              selected={endDateOnModal}
+              onChange={(date) => setEndDateOnModal(date)}
+            ></DatePicker>
+          </div>
+          <div className="flex w-full justify-end">
+            <button
+              className="text-white bg-blue-500 text-lg px-4 py-2 rounded-md hover:bg-blue-600"
+              onClick={async () => {
+                // Taskの更新をしつつ、結果(更新されたTask)を変数resultに代入
+                const result: Task = await (
+                  await axios.put(`/api/tasks/update/${props.task.id}`, {
+                    name: nameOnModal,
+                    about: aboutOnModal,
+                    startDate: startDateOnModal,
+                    endDate: endDateOnModal,
+                  })
+                ).data;
+                // 変数resultの中身があれば、画面側にも反映する
+                if (result) {
+                  props.setTask(result);
+                  props.closeModalAction();
+                } else {
+                  alert("タスクを正常に更新出来ませんでした");
+                }
+              }}
+            >
+              保存する
+            </button>
+            <button
+              onClick={props.closeModalAction}
+              className="text-white bg-slate-500 text-lg px-4 py-2 rounded-md hover:bg-slate-600 ml-3"
+            >
+              キャンセル
+            </button>
+          </div>
         </div>
-      </div>
+      </ModalOverlay>
     </>
   );
 };
--- front/react_app/src/pages/cycle/task/TaskCard.tsx
+++ front/react_app/src/pages/cycle/task/TaskCard.tsx
@@ -3,8 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Task } from "@prisma/client";
 import axios from "axios";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import CheckBox from "../../../components/CheckBox";
 import { fixDateTzAndFormat } from "../../../utils/fixDateTzAndFormat";
 import EditTaskModal from "./EditTaskModal";
@@ -14,18 +12,9 @@ interface PropsIfc {
 }
 
 const TaskCard = (props: PropsIfc) => {
-  const [task, setTask] = useState(props.task);
   const [showModal, setShowModal] = useState(false);
-
   const [complete, setComplete] = useState(props.task.complete);
-  const [nameOnModal, setNameOnModal] = useState(props.task.name);
-  const [aboutOnModal, setAboutOnModal] = useState(props.task.about);
-  const [startDateOnModal, setStartDateOnModal] = useState(
-    props.task.startDate ? new Date(props.task.startDate) : null,
-  );
-  const [endDateOnModal, setEndDateOnModal] = useState(
-    props.task.endDate ? new Date(props.task.endDate) : null,
-  );
+  const [task, setTask] = useState(props.task);
 
   return (
     <>
@@ -73,76 +62,11 @@ const TaskCard = (props: PropsIfc) => {
         </div>
       </div>
       {showModal && (
-        <EditTaskModal closeModalAction={() => setShowModal(false)}>
-          <h1 className="text-xl">タスクの編集</h1>
-          <p className="my-2">タスク名</p>
-          <input
-            type="text"
-            className="w-full p-2 rounded-md bg-slate-200 border-none outline-none"
-            value={nameOnModal}
-            onChange={(e) => setNameOnModal(e.target.value)}
-            required
-          />
-          <p className="my-2">概要</p>
-          <input
-            type="text"
-            className="w-full p-2 rounded-md bg-slate-200 border-none outline-none"
-            value={aboutOnModal || ""}
-            onChange={(e) => setAboutOnModal(e.target.value)}
-            required
-          />
-          <p className="my-2">期間</p>
-          <div>
-            <DatePicker
-              className="w-1/3 p-2 rounded-md bg-slate-200 border-none outline-none"
-              dateFormat="yyyy/MM/dd"
-              selected={startDateOnModal}
-              onChange={(date) => {
-                setStartDateOnModal(date);
-              }}
-            ></DatePicker>
-            <span className="mx-2 text-xl">~</span>
-            <DatePicker
-              className="w-1/3 p-2 rounded-md bg-slate-200 border-none outline-none"
-              dateFormat="yyyy/MM/dd"
-              selected={endDateOnModal}
-              onChange={(date) => setEndDateOnModal(date)}
-            ></DatePicker>
-          </div>
-
-          <div className="flex w-full justify-end">
-            <button
-              className="text-white bg-blue-500 text-lg px-4 py-2 rounded-md hover:bg-blue-600"
-              onClick={async () => {
-                // Taskの更新をしつつ、結果(更新されたTask)を変数resultに代入
-                const result: Task = await (
-                  await axios.put(`/api/tasks/update/${props.task.id}`, {
-                    name: nameOnModal,
-                    about: aboutOnModal,
-                    startDate: startDateOnModal,
-                    endDate: endDateOnModal,
-                  })
-                ).data;
-
-                // 変数resultの中身があれば、画面側にも反映する
-                if (result) {
-                  setTask(result);
-                  setShowModal(false);
-                } else {
-                  alert("タスクを正常に更新出来ませんでした");
-                }
-              }}
-            >
-              保存する
-            </button>
-            <button
-              onClick={() => setShowModal(false)}
-              className="text-white bg-slate-500 text-lg px-4 py-2 rounded-md hover:bg-slate-600 ml-3"
-            >
-              キャンセル
-            </button>
-          </div>
-        </EditTaskModal>
+        <EditTaskModal
+          closeModalAction={() => setShowModal(false)}
+          task={props.task}
+          setTask={setTask}
+        ></EditTaskModal>
       )}
     </>
   );
