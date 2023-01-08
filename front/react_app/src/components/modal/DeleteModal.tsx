d382218fd9b9e63bfdbc143fd491771d64edcefb
katkatprog <katkatprog@gmail.com>
2023-01-08 12:13:23 +0000

    cycle削除のモーダルのオーバーレイに、切り出したModalOverlayを適用

--- front/react_app/src/components/modal/DeleteModal.tsx
+++ front/react_app/src/components/modal/DeleteModal.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteErasedCycle } from "../../redux/erasedCyclesSlice";
 import { hideModal } from "../../redux/modalSlice";
 import { RootState } from "../../redux/store";
-import Modal from "./Modal";
+import ModalOverlay from "./ModalOverlay";
 
 const DeleteModal = () => {
   const modalState = useSelector((state: RootState) => state.modal.value);
@@ -12,15 +12,37 @@ const DeleteModal = () => {
     // "削除する"が押された際の処理
     await axios.delete(`/api/cycles/${modalState.cycleId}/${1}`);
     dispatch(deleteErasedCycle(modalState.cycleId));
-    dispatch(hideModal());
+    closeModalAction();
   };
+
+  // Modalを閉じるアクション
+  const closeModalAction = () => dispatch(hideModal());
+
   return (
     <>
-      <Modal buttonMessage="削除" confirmAction={confirmAction}>
-        <p>{modalState.cycleName}を消去します。</p>
-        <p>削除すると元には戻せません。</p>
-        <p>本当によろしいですか。</p>
-      </Modal>
+      <ModalOverlay modalWidth="w-1/2" closeModalAction={closeModalAction}>
+        <div className="p-12">
+          <div className="flex flex-col items-center mb-6 text-lg">
+            <p>{modalState.cycleName}を削除します。</p>
+            <p>削除すると元には戻せません。</p>
+            <p>本当によろしいですか。</p>
+          </div>
+          <div>
+            <button
+              className="text-white bg-red-500 text-lg px-4 py-2 rounded-md hover:bg-red-600"
+              onClick={confirmAction}
+            >
+              削除
+            </button>
+            <button
+              onClick={closeModalAction}
+              className="text-white bg-slate-500 text-lg px-4 py-2 rounded-md hover:bg-slate-600 ml-6"
+            >
+              キャンセル
+            </button>
+          </div>
+        </div>
+      </ModalOverlay>
     </>
   );
 };
