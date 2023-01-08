f65b1a93b81c6323a7ca501a82be8bc0fa0fc87a
katkatprog <katkatprog@gmail.com>
2023-01-08 15:23:21 +0000

    サイクルをReduxではなくstateで管理するよう改修(1/n) slice削除

--- front/react_app/src/redux/store.ts
+++ front/react_app/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cycleSlice } from "./cycleSlice";
 import { cyclesSlice } from "./cyclesSlice";
 import { erasedCyclesSlice } from "./erasedCyclesSlice";
 import { menuSlice } from "./menuSlice";
 
 export const store = configureStore({
   reducer: {
-    cycle: cycleSlice.reducer,
     cycles: cyclesSlice.reducer,
     erasedCycles: erasedCyclesSlice.reducer,
     menu: menuSlice.reducer,
